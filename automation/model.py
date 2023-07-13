b06540c6f4fcb2346dc9728866152902ce95cec1
J. Nsofini <jnsofini@gmail.com>
2023-07-13 11:14:31 -0600

    Updated code quality for unused s3fs

--- automation/model.py
+++ automation/model.py
@@ -48,29 +48,42 @@ def load_model_mlflow(run_id):
 #     model = Scorecard.load(f"{model_path}/model.pkl")
 #     return model
 
+
 def load_with_scorecard(path):
     return Scorecard.load(path)
 
+
 # # Working code when tested locally
-# def load_model(run_id):
-#     model_path = get_model_location(run_id=run_id)
-#     if not model_path.startswith("s3://"):
-#         return load_with_scorecard(f"{model_path}/model.pkl")
-#     s3_file = S3FileSystem(
-#         anon=False,
-#         secret=os.getenv("AWS_SECRET_ACCESS_KEY"),
-#         key=os.getenv("AWS_ACCESS_KEY_ID"),
-#     )
-#     print(model_path)
-#     model = pickle.load(s3_file.open(f"{model_path}/model.pkl"))
-#     return model
+def load_model_with_s3fs(run_id):
+    """Loading model with boto3 package only
+
+    Args:
+        run_id (str): The path to the model, or so called key without the
+        file name.
+
+    Returns:
+        Scorecard: Scoring model
+    """
+
+    model_path = get_model_location(run_id=run_id)
+    if not model_path.startswith("s3://"):
+        return load_with_scorecard(f"{model_path}/model.pkl")
+    s3_file = S3FileSystem(
+        anon=False,
+        secret=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        key=os.getenv("AWS_ACCESS_KEY_ID"),
+    )
+    print(model_path)
+    model = pickle.load(s3_file.open(f"{model_path}/model.pkl"))
+    return model
+
 
 # New code using boto3
 def load_model(run_id):
     """Loading model with boto3 package only
 
     Args:
-        run_id (str): The path to the model, or so called key without the 
+        run_id (str): The path to the model, or so called key without the
         file name.
 
     Returns:
@@ -79,12 +92,12 @@ def load_model(run_id):
     model_path = get_model_location(run_id=run_id)
     if not model_path.startswith("s3://"):
         return load_with_scorecard(f"{model_path}/model.pkl")
-    
+
     s3client = boto3.client('s3')
     response = s3client.get_object(
-        Bucket=os.getenv('MODEL_BUCKET', 'moose-solutions-mlops-registry'), 
-        Key=f'scorecards/{run_id}/model.pkl'
-        )
+        Bucket=os.getenv('MODEL_BUCKET', 'moose-solutions-mlops-registry'),
+        Key=f'scorecards/{run_id}/model.pkl',
+    )
 
     body = response['Body'].read()
     model = pickle.loads(body)
@@ -97,6 +110,7 @@ def load_model(run_id):
     # print(model_path)
     # model = pickle.load(s3_file.open(f"{model_path}/model.pkl"))
     return model
+
 
 def base64_decode(encoded_data):
     decoded_data = base64.b64decode(encoded_data).decode('utf-8')
