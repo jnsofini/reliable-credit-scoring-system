6d6f03cae768dfa7204dbe9cbfef9e444f5fd15b
J. Nsofini <jnsofini@gmail.com>
2023-07-14 14:32:03 -0600

    Youndex index function

--- credit-risk-model/src/util.py
+++ credit-risk-model/src/util.py
@@ -1,6 +1,8 @@
+import numpy as np
 import pandas as pd
 from optbinning import BinningProcess, Scorecard
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve
 
 # from statsmodels.stats.outliers_influence import variance_inflation_factor
 # from patsy import dmatrices
@@ -52,11 +54,7 @@ def setup_binning(df, *, target=None, features=None, binning_fit_params=None):
     else:
         binning_features = df.columns.values
 
-    categorical_variables = (
-        df[binning_features]
-        .select_dtypes(include=["object", "category", "string"])
-        .columns.values
-    )
+    categorical_variables = _get_categorical_features(df[binning_features])
 
     return BinningProcess(
         categorical_variables=categorical_variables,
@@ -68,6 +66,14 @@ def setup_binning(df, *, target=None, features=None, binning_fit_params=None):
         min_prebin_size=10e-5,
         special_codes=SPECIAL_CODES,
     )
+
+
+def _get_categorical_features(df):
+    categorical_variables = df.select_dtypes(
+        include=["object", "category", "string"]
+    ).columns.values
+
+    return categorical_variables
 
 
 def get_best_feature_from_each_cluster(clusters):
@@ -159,3 +165,37 @@ def load_data(path, drop_cols=None):
         columns_to_drop = list(set(drop_cols).intersection(set(df.columns.values)))
         return df.drop(columns=columns_to_drop)
     return df
+
+
+def sensivity_specifity_cutoff(y_true, y_score):
+    '''Find data-driven cut-off for classification
+
+    Cut-off is determied using Youden's index defined as sensitivity + specificity - 1.
+
+    Parameters
+    ----------
+
+    y_true : array, shape = [n_samples]
+        True binary labels.
+
+    y_score : array, shape = [n_samples]
+        Target scores, can either be probability estimates of the positive class,
+        confidence values, or non-thresholded measure of decisions (as returned by
+        “decision_function” on some classifiers).
+
+    References
+    ----------
+
+    Ewald, B. (2006). Post hoc choice of cut points introduced bias to diagnostic research.
+    Journal of clinical epidemiology, 59(8), 798-801.
+
+    Steyerberg, E.W., Van Calster, B., & Pencina, M.J. (2011). Performance measures for
+    prediction models and markers: evaluation of predictions and classifications.
+    Revista Espanola de Cardiologia (English Edition), 64(9), 788-794.
+
+    Jiménez-Valverde, A., & Lobo, J.M. (2007). Threshold criteria for conversion of probability
+    of species presence to either–or presence–absence. Acta oecologica, 31(3), 361-369.
+    '''
+    fpr, tpr, thresholds = roc_curve(y_true, y_score)
+    idx = np.argmax(tpr - fpr)
+    return thresholds[idx]
