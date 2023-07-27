a50f3d126ab8410199f10fc6f31fecf47ee1c333
J. Nsofini <jnsofini@gmail.com>
2023-07-27 15:43:01 -0600

    added timing script

---
+++ credit-risk-model/src/tools/timers.py
@@ -0,0 +1,17 @@
+import time
+from typing import Callable
+
+def timeit(logger=None):
+        def wrapper(func: Callable):
+            def wrapper_function(*args, **kwargs):
+                start = start_time = time.perf_counter()
+                func(*args, **kwargs)
+                end = time.perf_counter()
+                msg = f"Time taken : {(end - start):.2} seconds"
+                if logger:
+                    logger(msg)
+                else:
+                    print(msg)
+            return wrapper_function
+        return wrapper
+
