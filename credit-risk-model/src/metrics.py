781b5df4a2598ac834d5fdfb7cf2628f619bc58f
J. Nsofini <jnsofini@gmail.com>
2023-08-01 10:10:53 -0600

    Resolved pylint.

--- credit-risk-model/src/metrics.py
+++ credit-risk-model/src/metrics.py
@@ -1,12 +1,16 @@
+"""
+Metrics to measure performance of model.
+"""
 import numpy as np
-from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.metrics import roc_auc_score
 
 
-def kolmogorov_smirnov(y, y_pred):
+def kolmogorov_smirnov(y_true, y_pred):
+    # pylint: disable=invalid-name
     """Compute the Kolmogorov-Smirnov (KS).
     Parameters
     ----------
-    y : array-like, shape = (n_samples,)
+    y_true : array-like, shape = (n_samples,)
         Array with the target labels.
     y_pred : array-like, shape = (n_samples,)
         Array with predicted probabilities.
@@ -14,13 +18,13 @@ def kolmogorov_smirnov(y, y_pred):
         float: Summy count stats
     """
 
-    n_samples = y.shape[0]
-    n_event = np.sum(y)
+    n_samples = y_true.shape[0]
+    n_event = np.sum(y_true)
     n_nonevent = n_samples - n_event
 
     idx = y_pred.argsort()
-    yy = y[idx]
-    pp = y_pred[idx]
+    yy = y_true[idx] # pylint: disable=invalid-name
+    # pp = y_pred[idx]
 
     cum_event = np.cumsum(yy)
     cum_population = np.arange(0, n_samples)
@@ -35,25 +39,25 @@ def kolmogorov_smirnov(y, y_pred):
     return ks_score
 
 
-def ks(y, y_pred):
+def ks(y_true, y_pred):# pylint: disable=invalid-name
     """Compute the Kolmogorov-Smirnov (KS).
     Parameters
     ----------
-    y : array-like, shape = (n_samples,)
+    y_true : array-like, shape = (n_samples,)
         Array with the target labels.
     y_pred : array-like, shape = (n_samples,)
         Array with predicted probabilities.
     Returns:
         float: Summy count stats
     """
-    return kolmogorov_smirnov(y, y_pred)
+    return kolmogorov_smirnov(y_true, y_pred)
 
 
-def gini(y, y_pred):
+def gini(y_true, y_pred):
     """Compute the Gini using the roc_auc_score
     Parameters
     ----------
-    y : array-like, shape = (n_samples,)
+    y_true : array-like, shape = (n_samples,)
         Array with the target labels.
     y_pred : array-like, shape = (n_samples,)
         Array with predicted probabilities.
@@ -61,23 +65,23 @@ def gini(y, y_pred):
     Returns:
         float: Summy count stats
     """
-    auroc = roc_auc_score(y, y_pred)
+    auroc = roc_auc_score(y_true, y_pred)
     return auroc * 2 - 1
 
 
-def auc(y, y_pred):
+def auc(y_true, y_pred):
     """Compute the AUC.
     Parameters
     ----------
-    y : array-like, shape = (n_samples,)
+    y_true : array-like, shape = (n_samples,)
         Array with the target labels.
     y_pred : array-like, shape = (n_samples,)
         Array with predicted probabilities.
     """
-    return roc_auc_score(y, y_pred)
+    return roc_auc_score(y_true, y_pred)
 
 
-def formatted_metrics(y, y_pred):
+def formatted_metrics(y_true, y_pred):
     """Gets the fit stats used in the model building.
 
     These statistics includes:
@@ -87,7 +91,7 @@ def formatted_metrics(y, y_pred):
 
     Parameters
     ----------
-    y : array-like, shape = (n_samples,)
+    y_true : array-like, shape = (n_samples,)
         Array with the target labels.
     y_pred : array-like, shape = (n_samples,)
         Array with predicted probabilities.
@@ -95,9 +99,9 @@ def formatted_metrics(y, y_pred):
     Returns:
         dict: Summy count stats
     """
-    auc_ = auc(y, y_pred)
-    gini_ = gini(y, y_pred)
-    ks_ = ks(y, y_pred)
+    auc_ = auc(y_true, y_pred)
+    gini_ = gini(y_true, y_pred)
+    ks_ = ks(y_true, y_pred)
 
     return {
         "auc": f"{float(auc_):.2%}",
@@ -106,7 +110,7 @@ def formatted_metrics(y, y_pred):
     }
 
 
-def get_population_dist(y):
+def get_population_dist(y_true):
     """Takes an array and return stats.
 
     The stats include
@@ -116,13 +120,13 @@ def get_population_dist(y):
      - ratio of events to non-events
 
     Args:
-        y (array | list): Events and non events
+        y_true (array | list): Events and non events
 
     Returns:
         dict: Summy count stats
     """
-    pop_count = len(y)
-    default_count = sum(y)
+    pop_count = len(y_true)
+    default_count = sum(y_true)
     return {
         "Num of observations": int(pop_count),
         "Num of non-events": int(pop_count - default_count),
