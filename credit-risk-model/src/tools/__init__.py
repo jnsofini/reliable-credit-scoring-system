87e7f83f22a030dfd888e641819f4e7a674f9093
J. Nsofini <jnsofini@gmail.com>
2023-08-02 08:39:51 -0600

    Added setdir to module

--- credit-risk-model/src/tools/__init__.py
+++ credit-risk-model/src/tools/__init__.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-module-docstring
 
-from .artifacts_io import read_json, save_dict_to_json
+from .artifacts_io import read_json, save_dict_to_json, set_destination_directory
 from .formating import stage_info
 from .timers import timeit
--- credit-risk-model/src/tools/artifacts_io.py
+++ credit-risk-model/src/tools/artifacts_io.py
@@ -1,6 +1,28 @@
 """Module for loading and saving data to various formats."""
 
 import json
+from pathlib import Path
+from omegaconf import DictConfig
+
+
+def set_destination_directory(
+    cfg: DictConfig, pred_stage: str, stage: str, logger
+) -> list[Path]:
+    """Prepares the directories.
+
+    Args:
+        cfg (DictConfig): Configuration data
+
+    Returns:
+        list[Path]: List of directories
+    """
+    root_dir = Path(cfg.data.source).joinpath(cfg.data.test_dir)
+    predecessor_dir = root_dir.joinpath(pred_stage)
+    destination_dir = root_dir.joinpath(stage)
+    destination_dir.mkdir(parents=True, exist_ok=True)
+    logger(f"Working dir is:  {destination_dir}")
+
+    return predecessor_dir, destination_dir, root_dir
 
 
 def save_dict_to_json(data, filename, **kwargs):
