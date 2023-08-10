1cfea98c0eb1f54114f259733777d4b59c719bd7
J. Nsofini <jnsofini@gmail.com>
2023-08-10 11:36:01 -0600

    Added function to get model location and renamed some strings

--- monitoring/monitoring.py
+++ monitoring/monitoring.py
@@ -17,6 +17,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]: %(m
 SEND_TIMEOUT = 10
 
 BASE_PATH = Path(__file__).parent
+PROJECT_ROOT = BASE_PATH.parent
 
 DATA_BASE_PATH = BASE_PATH.parent/"data"
 
@@ -51,34 +52,44 @@ DATA_BASE="test_prepare"
 
 sync_engine = f"""postgresql+psycopg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{DATA_BASE}"""
 
-def prepare_database(conn_str:str | None = None, dbname="test"):
-    conn_str = conn_str or f"host={POSTGRES_HOST} port={POSTGRES_PORT} user={POSTGRES_USER} password={POSTGRES_PASSWORD}"
+def prepare_database(postgres_conn_str:str | None = None, dbname="test"):
+    postgres_conn_str = postgres_conn_str or f"host={POSTGRES_HOST} port={POSTGRES_PORT} user={POSTGRES_USER} password={POSTGRES_PASSWORD}"
 
     """Create database if it doesn't exist."""
-    with psycopg.connect(conn_str, autocommit=True) as conn:
+    with psycopg.connect(postgres_conn_str, autocommit=True) as conn:
         res = conn.execute(f"SELECT 1 FROM pg_database WHERE datname='{dbname}'")
         if len(res.fetchall()) == 0:
             conn.execute(f"create database {dbname};")
-        with psycopg.connect(f"{conn_str} dbname={dbname}") as conn:
+        with psycopg.connect(f"{postgres_conn_str} dbname={dbname}") as conn:
             conn.execute(create_table_statement)
 
 
-def calculate_metrics_psi(model, X_actual, y_actual, X_expected, y_expected): 
+def calculate_metrics_psi(model, x_actual, y_actual, x_expected, y_expected): 
     monitoring = ScorecardMonitoring(scorecard=model, psi_method="cart", psi_n_bins=10, verbose=True)
-    monitoring.fit(X_actual=X_actual, y_actual=y_actual, X_expected=X_expected, y_expected=y_expected)
+    monitoring.fit(X_actual=x_actual, y_actual=y_actual, X_expected=x_expected, y_expected=y_expected)
     psi_table = monitoring.psi_table()
     # psi_table = _format_psi_table(psi_table)
     psi_feature_table = monitoring.psi_variable_table()
     
     return psi_table, psi_feature_table.rename(columns=str.lower) #.to_dict(orient="records")
 
+def get_model_location():
+     """Gets the location of the model"""
+     return os.getenv("MODEL_LOCATION", DATA_BASE_PATH / "model/model.pkl")
 
 def monitoring_psi():
     # dbname="test-prepare"
     prepare_database(dbname=DATA_BASE)
     x_expected, x_actual, y_expected, y_actual = get_data()
-    score_card_model = Scorecard.load("/home/fini/github-projects/reliable-credit-scoring-system/credit-risk-model/model/model.pkl")
-    score_psi, feature_psi = calculate_metrics_psi(score_card_model, x_actual, y_actual.values, x_expected, y_expected.values)
+    model_location = get_model_location()
+    score_card_model = Scorecard.load(model_location)
+    score_psi, feature_psi = calculate_metrics_psi(
+         model=score_card_model, 
+         x_actual=x_actual, 
+         y_actual=y_actual.values, 
+         x_expected=x_expected, 
+         y_expected=y_expected.values
+         )
     storage.insert_dataframe(
         table_name="score_psi", 
         table_data=score_psi, 
